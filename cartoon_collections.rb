98739c3394abeacb81f10aee051dd5c66191b7b5
Asif Khan <akhan@codeinreach.com>
2019-07-14 19:02:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+    arr.each_with_index() do |name, i|
+      puts ">#{i+1}. #{name}"
+      i+=1
+    end
+  end
+
+def summon_captain_planet(arr)
+  arr.map do |element|
+  element.capitalize + "!"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  p arr.any? { |word| word.length > 4 }
 end
 
 def find_the_cheese# code an argument here
